f1199fb665672969c30905b4dbfd1c0a04748eb8
wjdghks950 <jeonghwankim123@gmail.com>
2023-11-07 20:04:15 -0600

    Update script - add use_prompt_type_case and use_gold_coarse

--- llava/eval/run_eval_inaturalist.sh
+++ llava/eval/run_eval_inaturalist.sh
@@ -3,7 +3,15 @@
 echo "task_type_id [\"high_coarse: 0\", \"coarse: 1\", \"fine: 2\"] >> "
 read task_type_id
 
-echo "prompt_type_id [\"cot_0shot: 0\", \"cot_fewshot: 1\", \"attr_seek: 2\"] >> "
-read prompt_type_id
+echo "use_prompt [true/false] >> "
+read use_prompt
 
-python eval_inaturalist.py --task_type_id $task_type_id --prompt_type_id $prompt_type_id
+# Only ask for prompt_type_id if use_prompt is True
+if [ "$use_prompt" == true ]; then
+	echo "prompt_type_id [\"cot_0shot: 0\", \"cot_fewshot: 1\", \"attr_seek: 2\"] >> "
+	read prompt_type_id
+	python eval_inaturalist.py --task_type_id $task_type_id --prompt_type_id $prompt_type_id --use_prompt
+else
+	prompt_type_id=0  # Set prompt_type_id to an empty string if use_prompt is not True
+	python eval_inaturalist.py --task_type_id $task_type_id
+fi
