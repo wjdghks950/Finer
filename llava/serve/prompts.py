90da889f1722f2b0fc22599c09f47416ca1132f8
wjdghks950 <jeonghwankim123@gmail.com>
2024-01-06 00:27:20 -0600

    Updated prompts for attribute generation and added prompt for attr_gen_wiki

--- llava/serve/prompts.py
+++ llava/serve/prompts.py
@@ -41,28 +41,79 @@ Provide your answer after "Answer:" and make sure to follow the binomial nomencl
 Let's think step by step. 
 '''
 
+# TODO: Ansel's original prompt
+# attr_gen = '''
+# What are useful visual features for distinguishing {concept_placeholder} in a photo? Provide the answer as lists of required and likely attributes. For example, for a school bus you might say:
+# Required:
+# - yellow
+# - black
+# - wheels
+# - windows
+# - bus
+
+# Likely:
+# - school children
+# - stop sign
+# - school bus lights
+
+# Provide your response in the above format, saying nothing else. If there are no useful visual features, simply write "none". For example, if there are no useful required features, say:
+
+# Required:
+# none
+# '''
+
+
+# Revised version of prompt
 attr_gen = '''
-What are useful visual features for distinguishing {concept_placeholder} in a photo? Provide the answer as lists of required and likely attributes. For example, for a school bus you might say:
+What are useful visual features for distinguishing {concept_placeholder} in a photo? \
+Provide the answer as lists of required and likely attributes. For example, for a bengal tiger (Felis Tigris) you might say:
+
 Required:
-- yellow
-- black
-- wheels
-- windows
-- bus
+- yellow to light orange coat
+- dark brown to black stripes
+- black rings on the tail
+- inner legs and belly are white
+- 21 to 29 stripes
 
 Likely:
-- school children
-- stop sign
-- school bus lights
+- lives in mangrove, wooded habitat
+- amber, yellow eyes
+- large, padded paws
+- long tail
+- stout teeth
 
-Provide your response in the above format, saying nothing else. If there are no useful visual features, simply write "none". For example, if there are no useful required features, say:
-
-Required:
-none
+In the required (Required:) set, do not include relative attributes like size or weight. \
+Provide your response in the above format, saying nothing else. If there are no useful visual features, simply write "none". \
 '''
 
+# Prompt to extract descriptive, physical attributes that pertain to the organism's Wikipedia page
+attr_gen_wiki ='''
+What are useful visual features for distinguishing {concept_placeholder} in a photo? \
+Given an input document (Document:) that may talk about {concept_placeholder}, provide the answer as lists of required and likely attributes. \
+For example, for a bengal tiger (Felis Tigris) you might say:
+
+Required:
+- yellow to light orange coat
+- dark brown to black stripes
+- black rings on the tail
+- inner legs and belly are white
+- 21 to 29 stripes
+
+Likely:
+- lives in mangrove, wooded habitat
+- amber, yellow eyes
+- large, padded paws
+- long tail
+- stout teeth
+
+In the required (Required:) set, do not include relative attributes like size or weight. \
+If no document is given, generate from what you already know about {concept_placeholder}.
+Provide your response in the above format, saying nothing else. If there are no useful visual features, simply write "none".
+'''
+
+
 attr_gen_image = '''
-What are useful visual features for distinguishing this organism in the photo? Provide the answer as lists of required and likely attributes. For example, for a school bus you might say:
+What are useful visual features for distinguishing the {concept_placeholder} in the photo? Provide the answer as lists of required and likely attributes. For example, for a school bus you might say:
 Required:
 - yellow
 - black
@@ -92,7 +143,4 @@ PROMPT_DICT['cot_0shot_coarse'] = cot_0shot_coarse.strip()
 PROMPT_DICT['cot_0shot_fine'] = cot_0shot_fine.strip()
 PROMPT_DICT['attr_gen'] = attr_gen.strip()
 PROMPT_DICT['attr_gen_image'] = attr_gen_image.strip()
-
-def prompt_parser(text, parse_mode='attr_gen'):
-    if parse_mode == 'attr_gen':
-        pass
+PROMPT_DICT['attr_gen_wiki'] = attr_gen_wiki.strip()
