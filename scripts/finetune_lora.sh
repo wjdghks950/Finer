e8afabe245c6b314f116ebacb443104a520330d1
wjdghks950 <jeonghwankim123@gmail.com>
2024-02-04 01:08:06 -0600

    Modify fineunte scripts

--- scripts/finetune_lora.sh
+++ scripts/finetune_lora.sh
@@ -5,8 +5,8 @@
 # Uncomment and set the following variables correspondingly to run this script:
 
 ################## VICUNA ##################
-# PROMPT_VERSION=v1
-# MODEL_VERSION="vicuna-v1-3-7b"
+PROMPT_VERSION=v1
+MODEL_VERSION="vicuna-v1-3-7b"
 ################## VICUNA ##################
 
 ################## LLaMA-2 ##################
@@ -17,21 +17,21 @@
 deepspeed llava/train/train_mem.py \
     --deepspeed ./scripts/zero2.json \
     --lora_enable True \
-    --model_name_or_path ./checkpoints/$MODEL_VERSION \
+    --model_name_or_path ../../checkpoints/$MODEL_VERSION \
     --version $PROMPT_VERSION \
-    --data_path ./playground/data/llava_instruct_80k.json \
+    --data_path ../../playground/data/llava_instruct_80k.json \
     --image_folder /path/to/coco/train2017 \
     --vision_tower openai/clip-vit-large-patch14 \
-    --pretrain_mm_mlp_adapter ./checkpoints/llava-$MODEL_VERSION-pretrain/mm_projector.bin \
+    --pretrain_mm_mlp_adapter ../../checkpoints/llava-$MODEL_VERSION-pretrain/mm_projector.bin \
     --mm_vision_select_layer -2 \
     --mm_use_im_start_end False \
     --mm_use_im_patch_token False \
-    --bf16 True \
-    --output_dir ./checkpoints/llava-$MODEL_VERSION-finetune_lora \
+    --bf16 False \
+    --output_dir ../../checkpoints/llava-$MODEL_VERSION-finetune_lora \
     --num_train_epochs 1 \
-    --per_device_train_batch_size 16 \
+    --per_device_train_batch_size 1 \
     --per_device_eval_batch_size 4 \
-    --gradient_accumulation_steps 1 \
+    --gradient_accumulation_steps 16 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
     --save_steps 50000 \
@@ -41,7 +41,7 @@ deepspeed llava/train/train_mem.py \
     --warmup_ratio 0.03 \
     --lr_scheduler_type "cosine" \
     --logging_steps 1 \
-    --tf32 True \
+    --tf32 False \
     --model_max_length 2048 \
     --gradient_checkpointing True \
     --lazy_preprocess True \
--- scripts/v1_5/finetune.sh
+++ scripts/v1_5/finetune.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
 
-deepspeed ../../llava/train/train.py \
-    --deepspeed ../zero3.json \
-    --model_name_or_path lmsys/vicuna-7b-v1.5 \
+deepspeed llava/train/train_mem.py \
+    --deepspeed ./scripts/zero3.json \
+    --model_name_or_path lmsys/vicuna-13b-v1.5 \
     --version v1 \
-    --data_path ../../playground/data/llava_v1_5_mix665k.json \
+    --data_path ../../playground/data/llava_v1_5_mix865k_inat2021.json \
     --image_folder ../../playground/data \
     --vision_tower openai/clip-vit-large-patch14-336 \
-    --pretrain_mm_mlp_adapter ../../checkpoints/llava-v1.5-mlp2x-336px-pretrain-vicuna-7b-v1.5/mm_projector.bin \
+    --pretrain_mm_mlp_adapter ../../checkpoints/llava-v1.5-13b-pretrain/mm_projector.bin \
     --mm_projector_type mlp2x_gelu \
     --mm_vision_select_layer -2 \
     --mm_use_im_start_end False \
     --mm_use_im_patch_token False \
     --image_aspect_ratio pad \
     --group_by_modality_length True \
-    --bf16 False \
-    --fp16 True \
+    --bf16 True \
     --output_dir ../../checkpoints/llava-v1.5-7b-865k-inat2021 \
     --num_train_epochs 1 \
     --per_device_train_batch_size 1 \
     --per_device_eval_batch_size 1 \
-    --gradient_accumulation_steps 64 \
+    --gradient_accumulation_steps 16 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
     --save_steps 50000 \
@@ -30,9 +29,9 @@ deepspeed ../../llava/train/train.py \
     --warmup_ratio 0.03 \
     --lr_scheduler_type "cosine" \
     --logging_steps 1 \
-    --tf32 False \
+    --tf32 True \
     --model_max_length 2048 \
     --gradient_checkpointing True \
-    --dataloader_num_workers 1 \
+    --dataloader_num_workers 4 \
     --lazy_preprocess True \
     --report_to wandb
