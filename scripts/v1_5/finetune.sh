e144a06649dc842996e269e279790d306f0d9964
JiaxinQin0814 <2020201476@ruc.edu.cn>
2024-02-05 23:33:10 +0000

    Modify to train on vicuna-7b-1.5 projector

--- scripts/v1_5/finetune.sh
+++ scripts/v1_5/finetune.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-deepspeed llava/train/train_mem.py \
-    --deepspeed ./scripts/zero3.json \
-    --model_name_or_path lmsys/vicuna-13b-v1.5 \
+deepspeed --include localhost:2,3 ../../llava/train/train_mem.py \
+    --deepspeed ../zero3.json\
+    --model_name_or_path lmsys/vicuna-7b-v1.5 \
     --version v1 \
     --data_path ../../playground/data/llava_v1_5_mix865k_inat2021.json \
     --image_folder ../../playground/data \
     --vision_tower openai/clip-vit-large-patch14-336 \
-    --pretrain_mm_mlp_adapter ../../checkpoints/llava-v1.5-13b-pretrain/mm_projector.bin \
+    --pretrain_mm_mlp_adapter ../../checkpoints/llava-v1.5-mlp2x-336px-pretrain-vicuna-7b-v1.5/mm_projector.bin \
     --mm_projector_type mlp2x_gelu \
     --mm_vision_select_layer -2 \
     --mm_use_im_start_end False \
