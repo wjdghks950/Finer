38d9c94440f1ab0a40b52fd6cdbea915122c5d78
wjdghks950 <jeonghwankim123@gmail.com>
2024-02-08 21:22:48 -0600

    Remove --mm_projector_lr 2e-5 - causes error

--- scripts/v1_5/finetune_lora.sh
+++ scripts/v1_5/finetune_lora.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
+# --mm_projector_lr 2e-5 \ - TODO: It has been removed!
 
-deepspeed llava/train/train_mem.py \
-    --lora_enable True --lora_r 128 --lora_alpha 256 --mm_projector_lr 2e-5 \
-    --deepspeed ./scripts/zero3.json \
-    --model_name_or_path lmsys/vicuna-13b-v1.5 \
+deepspeed ../../llava/train/train.py \
+    --lora_enable True --lora_r 128 --lora_alpha 256 \
+    --deepspeed ../zero3.json \
+    --model_name_or_path lmsys/vicuna-7b-v1.5 \
     --version v1 \
-    --data_path ./playground/data/llava_v1_5_mix665k.json \
-    --image_folder ./playground/data \
+    --data_path ../../playground/data/llava_v1_5_mix865k_inat2021.json \
+    --image_folder ../../playground/data \
     --vision_tower openai/clip-vit-large-patch14-336 \
-    --pretrain_mm_mlp_adapter ./checkpoints/llava-v1.5-13b-pretrain/mm_projector.bin \
+    --pretrain_mm_mlp_adapter ../../checkpoints/llava-v1.5-mlp2x-336px-pretrain-vicuna-7b-v1.5/mm_projector.bin \
     --mm_projector_type mlp2x_gelu \
     --mm_vision_select_layer -2 \
     --mm_use_im_start_end False \
     --mm_use_im_patch_token False \
     --image_aspect_ratio pad \
     --group_by_modality_length True \
-    --bf16 True \
-    --output_dir ./checkpoints/llava-v1.5-13b-lora \
+    --bf16 False \
+    --output_dir ../../checkpoints/llava-v1.5-7b-lora-865k-inat2021 \
     --num_train_epochs 1 \
     --per_device_train_batch_size 16 \
     --per_device_eval_batch_size 4 \
@@ -30,7 +31,7 @@ deepspeed llava/train/train_mem.py \
     --warmup_ratio 0.03 \
     --lr_scheduler_type "cosine" \
     --logging_steps 1 \
-    --tf32 True \
+    --tf32 False \
     --model_max_length 2048 \
     --gradient_checkpointing True \
     --dataloader_num_workers 4 \
